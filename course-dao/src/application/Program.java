ad3f4752c26895e9386f5ab9fdb2b13369ba9d91
Cunhaisis <isis.cunha13@gmail.com>
2020-11-14 12:44:54 -0300

    findById implementation

--- course-dao/src/application/Program.java
+++ course-dao/src/application/Program.java
@@ -11,12 +11,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Marketing");
-		Seller seller = new Seller(21, "Bruno Mars", "bruno@mars.com", new Date(), 3000.0, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDAO();
 		
-		System.out.println(obj);
+		Seller seller = sellerDao.findById(7);
+		
 		System.out.println(seller);
 	}
 
